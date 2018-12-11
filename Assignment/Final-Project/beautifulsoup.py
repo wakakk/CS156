4f9482a96e1c0c4b9a1dc20b58beba3594e240bd
Boyu Jiang <jbyjiangboyu@126.com>
2018-12-11 08:52:57 -0800

    14-1 update

--- Assignment/Final-Project/beautifulsoup.py
+++ Assignment/Final-Project/beautifulsoup.py
@@ -3,41 +3,42 @@ from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 import requests
 
-
 def ebay_url(keyword, page):
     ROOT = 'https://www.ebay.com/sch/i.html?'
     url = ROOT + urlencode({'_nkw': keyword, '_pgn': page})
     return url
 
+def parse_item_ebay(item):
+    product_link = item.find(class_="s-item__link").attrs['href']
+    pic_link = item.find(class_='s-item__image-img').attrs['src']
+    price = item.find(class_="s-item__price").text
+    name = item.find(class_="s-item__title").text
+    return {
+        'product_link': product_link,
+        'pic_link': pic_link,
+        'price': price,
+        'name': name,
+    }
+
 headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
 }
 
-result = []
-i = 1
-keyword = "hat"
+def main():
 
-page_link = ebay_url(keyword, i)
-page_response = requests.get(page_link, headers=headers).text
-page_content = BeautifulSoup(page_response, "html.parser")
+    result = []
+    i = 1
+    keyword = "hat"
 
+    page_link = ebay_url(keyword, i)
+    page_response = requests.get(page_link, headers=headers).text
+    page_content = BeautifulSoup(page_response, "html.parser")
 
-# if page_content.find(class_='srp-results') is not None:
-#     search_results = page_content.find(class_='srp-results').find_all(class_='s-item')
-#     for item in search_results:
-#         result.append(parse_item_ebay(item))
-#         if len(result) > max_num:
-#             result = result[:max_num]
-#             return result
-# elif page_content.find(class_='gv-ic full-width left') is not None:
-#     search_results = page_content.find(class_='gv-ic full-width left').find_all(class_='sresult gvresult')
-#     for item in search_results:
-#         result.append(parse_item_ebay_alternative(item))
-#         if len(result) > max_num:
-#             result = result[:max_num]
-#             return result
-# else:
-#     return result
-# i += 1
+    search_results = page_content.find(class_='srp-results').find_all(class_='s-item')
 
-print (page_content)
+    for item in search_results:
+        result.append(parse_item_ebay(item))
+
+    return result
+
+print (main())
